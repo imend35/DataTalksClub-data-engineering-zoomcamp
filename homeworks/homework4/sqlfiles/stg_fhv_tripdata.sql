7453c9b510e3ba0c21145ea300e1025bfc837619
Esila Nur DEMİRCİ <esila.nur.demirci@gmail.com>
2026-02-10 16:53:50 +0300

    Create CTE for renaming and casting tripdata columns
    Add CTE to rename and cast columns from source data.

--- homeworks/homework4/sqlfiles/stg_fhv_tripdata.sql
+++ homeworks/homework4/sqlfiles/stg_fhv_tripdata.sql
@@ -1,1 +1,28 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('raw', 'fhv_tripdata_2019') }}
+
+),
+
+renamed AS (
+
+    SELECT
+        dispatching_base_num,
+
+        SAFE_CAST(pickup_datetime AS TIMESTAMP)  AS pickup_datetime,
+        SAFE_CAST(dropoff_datetime AS TIMESTAMP) AS dropoff_datetime,
+
+        SAFE_CAST(PUlocationID AS INT64) AS pickup_location_id,
+        SAFE_CAST(DOlocationID AS INT64) AS dropoff_location_id,
+
+        affiliated_base_number
+
+    FROM source
+    WHERE dispatching_base_num IS NOT NULL
+
+)
+
+SELECT *
+FROM renamed
 
