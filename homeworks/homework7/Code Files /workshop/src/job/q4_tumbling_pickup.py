25783af29430f5ea5a060439e840995bbc96316b
Esila Nur DEMİRCİ <esila.nur.demirci@gmail.com>
2026-03-12 22:48:22 +0300

    Add Flink job for tumbling window aggregation
    Implement Flink job to process green trip data from Kafka and store results in PostgreSQL.

--- homeworks/homework7/Code Files /workshop/src/job/q4_tumbling_pickup.py
+++ homeworks/homework7/Code Files /workshop/src/job/q4_tumbling_pickup.py
@@ -1,1 +1,69 @@
 
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
+
+
+def main():
+
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    source_ddl = """
+        CREATE TABLE green_trips (
+            lpep_pickup_datetime STRING,
+            lpep_dropoff_datetime STRING,
+            PULocationID DOUBLE,
+            DOLocationID DOUBLE,
+            passenger_count DOUBLE,
+            trip_distance DOUBLE,
+            tip_amount DOUBLE,
+            total_amount DOUBLE,
+
+            event_timestamp AS TO_TIMESTAMP(lpep_pickup_datetime, 'yyyy-MM-dd HH:mm:ss'),
+            WATERMARK FOR event_timestamp AS event_timestamp - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'green-trips',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'scan.startup.mode' = 'earliest-offset',
+            'format' = 'json'
+        )
+    """
+
+    sink_ddl = """
+        CREATE TABLE q4_tumbling (
+            window_start TIMESTAMP(3),
+            PULocationID INT,
+            num_trips BIGINT,
+            PRIMARY KEY (window_start, PULocationID) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = 'q4_tumbling',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        )
+    """
+
+    t_env.execute_sql(source_ddl)
+    t_env.execute_sql(sink_ddl)
+
+    t_env.execute_sql("""
+        INSERT INTO q4_tumbling
+        SELECT
+            window_start,
+            CAST(PULocationID AS INT),
+            COUNT(*)
+        FROM TABLE(
+            TUMBLE(TABLE green_trips, DESCRIPTOR(event_timestamp), INTERVAL '5' MINUTES)
+        )
+        GROUP BY window_start, CAST(PULocationID AS INT)
+    """)
+
+
+if __name__ == "__main__":
+    main()
