5459dc6dbad3fbb964a754f9245951cad03ee2bd
Esila Nur DEMİRCİ <esila.nur.demirci@gmail.com>
2026-03-15 15:21:38 +0300

    Create stg_ai_repositories.sql

--- project1/ai-open-source-intelligence-platform/dbt/models/staging/stg_ai_repositories.sql
+++ project1/ai-open-source-intelligence-platform/dbt/models/staging/stg_ai_repositories.sql
@@ -1,3 +1,12 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "created_at",
+      "data_type": "timestamp"
+    },
+    cluster_by=["language","subject"]
+) }}
+
 SELECT
    *
 FROM `braided-keel-490209-q8.ai_open_source_dw.ai_repositories`
