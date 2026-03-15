81bec5c905baca49aafb608b1cf6169998e5abd4
Esila Nur DEMİRCİ <esila.nur.demirci@gmail.com>
2026-03-15 15:01:22 +0300

    Create ai_repo_ai_type.sql

--- project1/ai-open-source-intelligence-platform/dbt/models/marts/ai_repo_ai_type.sql
+++ project1/ai-open-source-intelligence-platform/dbt/models/marts/ai_repo_ai_type.sql
@@ -1,3 +1,12 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "created_at",
+      "data_type": "timestamp"
+    },
+    cluster_by=["language","subject"]
+) }}
+
 SELECT
     subject ,
     COUNT(DISTINCT repo_name) AS repo_count
